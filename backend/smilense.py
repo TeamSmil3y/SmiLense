6c88586a23fd70026621202c2fda07dc4beb5d84
Tim Ruppert <tim.ruppert@cobiodata.com>
2024-05-14 17:41:32 +0200

    DB

--- backend/smilense.py
+++ backend/smilense.py
@@ -1,15 +1,52 @@
 import yaml
+import base64
 from pigeon.shortcuts import Log
-import sqalchemy as sqa
+import sqlalchemy as sql
+from sqlalchemy import Table, Column
 
 log = Log('SMILESENSE', 'blue')
 
+# create sqlalchemy engine to interact with db
+db_engine=sql.create_engine('sqlite:///./sqlite.db')
+metadata = sql.MetaData()
+metadata.reflect(bind=db_engine)
 
-def compare(data, files):
-	log.info(files)
+licenses = Table(
+	'licenses',
+	metadata,
+	*(
+		Column('key', sql.Text, primary_key=True),
+		Column('name', sql.Text),
+		Column('short_name', sql.Text),
+		Column('category',sql.Text),
+		Column('url', sql.Text),
+		Column('raw', sql.Text),
+	)
+)
+
+with db_engine.connect() as connection:
+	connection.execute(...)
+	connection.commit()
+
+def compare(data):
+	"""
+	Compares the license in the data to the configuration in the data.
+
+	:param data: pigeon HTTPRequest data
+	:return:
+	"""
 	log.info(data)
-	#config = yaml.safe_load(data)
+	config = yaml.safe_load(base64.b64decode(data.config))
+	license = base64.b64decode(data.config)
+	dependencies = base64.b64decode(data.dependencies)
+
 	#log.info(config)
+
+
+
 	return {'status': 1, 'compatability': 3, 'message': '...'}
 
 
+
+def check_match_known():
+	...
